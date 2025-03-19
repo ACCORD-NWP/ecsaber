3e79218c388419e2d0e89daae7102c28b813e4e6
Benjamin Menetrier <benjamin.menetrier@met.no>
2025-03-19 12:03:20 +0100

    Split BGOS into application+implementation

--- oops_patch/src/oops/runs/BGOS.h
+++ oops_patch/src/oops/runs/BGOS.h
@@ -7,47 +7,21 @@
 
 #pragma once
 
-#include <cmath>
-#include <fstream>
 #include <string>
 
 #include "eckit/config/Configuration.h"
-#include "eckit/config/LocalConfiguration.h"
-#include "oops/assimilation/ControlIncrement.h"
-#include "oops/assimilation/ControlVariable.h"
-#include "oops/assimilation/CostFunction.h"
-#include "oops/assimilation/CostJo.h"
-#include "oops/assimilation/DualVector.h"
-#include "oops/assimilation/HMatrix.h"
 #include "oops/assimilation/instantiateCostFactory.h"
 #include "oops/assimilation/instantiateMinFactory.h"
-#include "oops/base/Departures.h"
-#include "oops/base/Increment4D.h"
 #include "oops/base/instantiateCovarFactory.h"
 #include "oops/generic/instantiateObsErrorFactory.h"
 #include "oops/generic/instantiateTlmFactory.h"
-#include "oops/interface/Geometry.h"
-#include "oops/interface/Model.h"
-#include "oops/interface/State.h"
 #include "oops/runs/Application.h"
-#include "oops/util/ECUtilities.h"
-#include "oops/util/Logger.h"
+#include "oops/runs/BGOS_impl.h"
 
 namespace oops {
 
 template <typename MODEL>
 class BGOS : public Application {
-  using CostJo_ = CostJo<MODEL>;
-  using CtrlInc_ = ControlIncrement<MODEL>;
-  using CtrlVariable_ = ControlVariable<MODEL>;
-  using Departures_ = Departures<MODEL>;
-  using Dual_ = DualVector<MODEL>;
-  using Geometry_ = Geometry<MODEL>;
-  using HMatrix_ = HMatrix<MODEL>;
-  using Increment4D_ = Increment4D<MODEL>;
-  using Model_ = Model<MODEL>;
-  using State_ = State<MODEL>;
-
  public:
   // -----------------------------------------------------------------------------
   BGOS() {
@@ -61,131 +35,8 @@ class BGOS : public Application {
   virtual ~BGOS() {}
   // -----------------------------------------------------------------------------
   int execute(const eckit::Configuration& fullConfig) const {
-    // Setup resolution
-    const eckit::LocalConfiguration resolConfig(fullConfig, "resolution");
-    const Geometry_ resol(resolConfig);
-
-    // Setup model
-    const eckit::LocalConfiguration modelConfig(fullConfig, "model");
-    const Model_ model(resol, modelConfig);
-
-    // Setup cost function
-    const eckit::LocalConfiguration cfConf(fullConfig, "cost_function");
-    std::unique_ptr<CostFunction<MODEL> > J(
-        CostFactory<MODEL>::create(cfConf, resol, model));
-    Log::trace() << "BGOS: cost function has been set up" << std::endl;
-
-    // Initialize first guess from background
-    const CtrlVariable_ xx(J->jb().getBackground());
-
-    // Linearize cost function
-    PostProcessor<State_> post;
-    std::vector<eckit::LocalConfiguration> iterconfs;
-    fullConfig.get("variational.iteration", iterconfs);
-    J->linearize(xx, iterconfs[0], post);
-
-    // Get Jo
-    const CostJo_ & Jo = dynamic_cast<const CostJo_ &>(J->jterm(0));
-
-    // Setup ensemble configuration (if present) or randomization size
-    std::vector<eckit::LocalConfiguration> membersConfig;
-    size_t nens;
-    if (fullConfig.has("ensemble")) {
-      eckit::LocalConfiguration membersConfigTemplate;
-      fullConfig.get("ensemble", membersConfigTemplate);
-      nens = membersConfigTemplate.getInt("members");
-      util::expandEnsembleTemplate(membersConfigTemplate, nens);
-      membersConfig = membersConfigTemplate.getSubConfigurations("state");
-    } else {
-      fullConfig.get("randomization size", nens);
-    }
-    ASSERT(nens > 1);
-
-    // Setup linearized observation operator
-    const HMatrix_ HMatrix(*J);
-
-    // Setup dual vectors
-    ASSERT(J->nterms() == 1);
-    Dual_ mean;
-    Dual_ var;
-    mean.append(Jo.newDualVector());
-    var.append(Jo.newDualVector());
-
-    // Initialize mean and variance
-    mean.zero();
-    var.zero();
-
-    // Convert ensemble of backgrounds perturbations from primal to dual space
-    const bool nonlinear = fullConfig.getBool("use nonlinear observation operator", false);
-
-    for (size_t ie = 0; ie < nens; ++ie) {
-      // Get ensemble member
-      Increment4D_ dx4D(J->jb().jbState());
-      if (membersConfig.empty()) {
-        // Randomize member as increment
-        J->jb().jbState().randomize(dx4D);
-      } else {
-        // Read member as increment
-        dx4D[dx4D.first()].read(membersConfig[ie]);
-      }
-
-      // Convert to observation space
-      Dual_ dy;
-
-      if (nonlinear) {
-        // Use nonlinear observation operator
-
-        // Re-evaluate J around new state
-        CtrlVariable_ xxTmp(xx);
-        if (!membersConfig.empty()) {
-          xxTmp.state()[0].zero();       
-        }
-        xxTmp.state()[0] += dx4D[0];
-        J->evaluate(xxTmp, eckit::LocalConfiguration());
-
-        // Get departures
-        dy.append(Jo.newDepartures());
-      } else {
-        // Use linearized observation operator
-
-        // Setup control increment
-        CtrlInc_ dx(J->jb());
-        dx.state() = dx4D;
-
-        // Allocate dual vector
-        dy.append(Jo.newDualVector());
-
-        // Apply H matrix to control increment
-        HMatrix.multiply(dx, dy);
-      }
-
-      // Remove mean
-      dy -= mean;
-
-      // Update variance
-      if (ie > 0) {
-        const double fac = static_cast<double>(ie)/static_cast<double>(ie+1);
-        Dual_ dy2(dy);
-        dy2.schur_product_with(dy);
-        var.axpy(fac, dy2);
-      }
-
-      // Update mean
-      const double fac = 1.0/static_cast<double>(ie+1);
-      mean.axpy(fac, dy);
-    }
-
-    // Normalize variance
-    const double fac = 1.0/static_cast<double>(nens-1);
-    var *= fac;
-
-    // Write variance
-    const eckit::LocalConfiguration varianceConfig(fullConfig, "variance in observation space");
-    var.write(varianceConfig);
-
-    // Print global norm
-    Log::test() << "Variance in observation space norm: " << 
-      std::sqrt(var.dot_product_with(var)) << std::endl;
+    // Execute BGOS implementation
+    BGOS_impl<MODEL>(fullConfig);
 
     return 0;
   }
