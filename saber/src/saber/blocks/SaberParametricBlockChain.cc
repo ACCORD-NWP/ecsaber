176289f31ffb9d8022d04cbb35b129f0c308aab0
Benjamin Menetrier <benjamin.menetrier@met.no>
2026-02-09 16:28:48 +0100

    Remove debug prints

--- saber/src/saber/blocks/SaberParametricBlockChain.cc
+++ saber/src/saber/blocks/SaberParametricBlockChain.cc
@@ -104,7 +104,7 @@ oops::JediVariables SaberParametricBlockChain::initCentralBlock(
   // Set outer variables for central block
   const oops::JediVariables currentOuterVars = outerBlockChain_ ?
                              outerBlockChain_->innerVars() : outerVariables_;
-std::cout << "ok 1" << std::endl;
+
   // Get active variables
   oops::JediVariables activeVars = saberCentralBlockParams.getActiveVars(currentOuterVars);
 
@@ -115,8 +115,7 @@ std::cout << "ok 1" << std::endl;
                              "outer variables", Here());
     }
   }
-std::cout << "ok 2" << std::endl;
-std::cout << saberCentralBlockParams << std::endl;
+
   // Create central block
   centralBlock_ = std::make_unique<SaberCentralBlock>(outerGeom,
                                                       levelsAreTopDown,
@@ -125,7 +124,7 @@ std::cout << saberCentralBlockParams << std::endl;
                                                       saberCentralBlockParams,
                                                       fset4dXb[0],
                                                       fset4dFg[0]);
-std::cout << "ok 3" << std::endl;
+
   // Save central function space and variables
   centralFunctionSpace_ = outerGeom.functionSpace();
   centralVars_ = activeVars;
