2cf50c4a0f6a68368ddb6cb32eb148060b99e60c
Benjamin Menetrier <benjamin.menetrier@met.no>
2026-06-05 11:21:44 +0200

    Add internal/external interpolations

--- saber/src/saber/blocks/SaberEnsembleBlockChain.h
+++ saber/src/saber/blocks/SaberEnsembleBlockChain.h
@@ -68,6 +68,9 @@ class ScaleParameters : public oops::Parameters {
   // Localization parametric blockchain (optional)
   oops::OptionalParameter<eckit::LocalConfiguration> localizationParams{
     "localization", this};
+
+  // Localization at full resolution (even if an interpolator is present)
+  oops::Parameter<bool> locAtFullRes{"localization at full resolution", false, this};
 };
 
 
@@ -75,7 +78,10 @@ class ScaleParameters : public oops::Parameters {
 
 class ScaleData {
  public:
-  explicit ScaleData(const ScaleParameters & params) : params_(params) {}
+  explicit ScaleData(const ScaleParameters & params) :
+    params_(params),
+    internalInterpolation_(params_.interpolatorParams.value() && params_.locAtFullRes.value()),
+    externalInterpolation_(params_.interpolatorParams.value() && !params_.locAtFullRes.value()) {}
 
   // Accessors
   const std::unique_ptr<SaberOuterBlockChain> & filter() const
@@ -96,6 +102,10 @@ class ScaleData {
     {return ensemble_;}
   const ScaleParameters & params() const
     {return params_;}
+  const bool & internalInterpolation() const
+    {return internalInterpolation_;}
+  const bool & externalInterpolation() const
+    {return externalInterpolation_;}
 
  private:
   /// @brief Filter outer block chain.
@@ -108,6 +118,10 @@ class ScaleData {
   std::unique_ptr<oops::FieldSets> ensemble_;
   /// @brief Scale parameters
   const ScaleParameters params_;
+  /// @brief Internal interpolation
+  bool internalInterpolation_;
+  /// @brief External interpolation
+  bool externalInterpolation_;
 };
 
 // -----------------------------------------------------------------------------
@@ -471,8 +485,8 @@ SaberEnsembleBlockChain::SaberEnsembleBlockChain(const oops::Geometry<MODEL> & g
     }
 
     // Consistency check 2: residual from filter is incompatible with interpolator
-    if (scaleData.params().residualFromFilter.value()) {
-      ASSERT(!scaleData.interpolator());
+    if (scaleParams.residualFromFilter.value()) {
+      ASSERT(!scaleParams.interpolatorParams.value());
     }
 
     // Get interpolator outer geometry data
@@ -524,6 +538,10 @@ SaberEnsembleBlockChain::SaberEnsembleBlockChain(const oops::Geometry<MODEL> & g
         == util::getGridUid(interpolatorOuterGeomData.functionSpace()));
     }
 
+    // Get localization geometry data
+    const oops::GeometryData & localizationGeomData = scaleData.externalInterpolation() ?
+       filterOuterGeomData : interpolatorOuterGeomData;
+
     // Localization
     if (scaleParams.localizationParams.value()) {
       oops::Log::info() << "Info     : Localization setup" << std::endl;
@@ -538,7 +556,7 @@ SaberEnsembleBlockChain::SaberEnsembleBlockChain(const oops::Geometry<MODEL> & g
       // the current geometryData, using a generic constructor of the parametric block chain.
 
       // Check consistency of `geom` and the current geometry
-      const auto & currentFspace = filterOuterGeomData.functionSpace();
+      const auto & currentFspace = localizationGeomData.functionSpace();
       if (util::getGridUid(geom.geometry().functionSpace()) != util::getGridUid(currentFspace)) {
         oops::Log::info() << "Info     : Localization and ensemble are on different "
                              "functionSpaces, building localization with generic "
@@ -546,7 +564,7 @@ SaberEnsembleBlockChain::SaberEnsembleBlockChain(const oops::Geometry<MODEL> & g
         // Note QUENCH could just build another geometry here and use the standard
         // constructor, but other models usually don't have this ability to create a
         // Geometry on any mesh.
-        scaleData.localization() = std::make_unique<SaberParametricBlockChain>(filterOuterGeomData,
+        scaleData.localization() = std::make_unique<SaberParametricBlockChain>(localizationGeomData,
                                                                      currentOuterVars,
                                                                      fset4dXb,
                                                                      fset4dFg,
