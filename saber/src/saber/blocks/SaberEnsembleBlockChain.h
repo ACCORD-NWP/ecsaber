26ea99e643a51bd96f0b2166d22a9afea46beeea
Benjamin Menetrier <benjamin.menetrier@met.no>
2026-06-11 10:57:22 +0200

    Simplifies generic ensemble reading

--- saber/src/saber/blocks/SaberEnsembleBlockChain.h
+++ saber/src/saber/blocks/SaberEnsembleBlockChain.h
@@ -705,14 +705,25 @@ SaberEnsembleBlockChain::SaberEnsembleBlockChain(const oops::Geometry<MODEL> & g
 
           // No interpolator allowed
           ASSERT(!scaleData.interpolator());
-        }
 
-        // Read ensemble using model reader
-        scaleData.ensemble() = std::make_unique<oops::FieldSets>(readEnsemble(
-                                 geom,
-                                 scaleData.localization()->outerVariables(),
-                                 fset4dXb.times(), fset4dXb.commTime(), fset4dXb.commEns(),
-                                 scaleData.params().toConfiguration()));
+          // Read ensemble using model reader
+          scaleData.ensemble() = std::make_unique<oops::FieldSets>(readEnsemble(
+                                   geom,
+                                   scaleData.localization()->outerVariables(),
+                                   fset4dXb.times(), fset4dXb.commTime(), fset4dXb.commEns(),
+                                   scaleData.params().toConfiguration()));
+        } else if (scaleData.params().ensemblePertOtherGeom.value()) {
+          // Get current geometry
+          const oops::GeometryData & ensGeom = scaleData.interpolator() ?
+            scaleData.interpolator()->innerGeometryData() : outerBlockChain_ ?
+            outerBlockChain_->innerGeometryData() : util::geomData(geom);
+
+          // Read ensemble
+          scaleData.ensemble() = std::make_unique<oops::FieldSets>(ensGeom.functionSpace(),
+                                  currentOuterVars, fset4dXb.times(),
+                                  *scaleData.params().ensemblePertOtherGeom.value(),
+                                  ensGeom.comm(), fset4dXb.commTime());
+        }
       }
     }
   } else {
